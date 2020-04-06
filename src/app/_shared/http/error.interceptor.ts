84ee4bad2aed65f77b98b4b044d806271bf8cdfb
Kamil Karpus <kkarpus@omnis.pl>
2020-04-06 12:05:08 +0200

    error interceptor

--- src/app/_shared/http/error.interceptor.ts
+++ src/app/_shared/http/error.interceptor.ts
@@ -2,7 +2,7 @@ import { Observable, throwError, empty } from 'rxjs';
 import { HttpErrorResponse, HttpEvent, HttpHandler,HttpInterceptor, HttpRequest } from '@angular/common/http';
 
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
 import { UserService } from 'src/app/_services/user/user.service';
 import { RefreshTokenModel } from 'src/app/_models/user/RefreshToken.model';
 import { responseWithRefreshTokenModel } from 'src/app/_models/response/responseWithRefreshToken.model';
@@ -12,20 +12,27 @@ export class ErrorInterceptor implements HttpInterceptor {
 
   constructor(public userService: UserService){}
 
+
+  private addToken(request: HttpRequest<any>, token: string) {
+    return request.clone({
+        setHeaders: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((err: HttpErrorResponse) => {
         if (err.status == 401) {
           
-          this.userService.refreshToken(new RefreshTokenModel(localStorage.getItem('refreshToken')))
-          .subscribe(data=>{
-            console.log(data);
+          return this.userService.refreshToken(new RefreshTokenModel(localStorage.getItem('refreshToken')))
+          .pipe(
+            switchMap((data : responseWithRefreshTokenModel) =>{
             localStorage.setItem('token', data.token);
             localStorage.setItem('refreshToken', data.refreshToken); 
-            return next.handle(req);
-          })
-          
-          // return empty();
+            return next.handle(this.addToken(req, data.token));
+            }))
         } 
         else {
           return throwError(err);
